75ab266bcd46fd015447d16e3b6283eb18091358
knaveen.usapp@gmail.com <sudhakar.katakara@gmail.com>
2017-09-30 16:19:12 -0500

    Printing format file

---
+++ Basics/Print_format.py
@@ -0,0 +1,24 @@
+###Printing output in different options
+
+a=1
+st1= "one"
+
+print " first format of a and string are " , a ,st1
+
+###o/p is
+#first format of a and string are  1 one
+
+print"Integer value is %d and string value is %s" %(a,st1)
+
+###o/p is
+#Integer value is 1 and string value is one
+
+print " {} is value of a and {} value is String".format(a,st1)
+
+##o/p is
+# 1 is value of a and one value is String
+
+print "{0} is string value and {1} is a value".format(st1,a)
+
+### o/p is
+# one is string value and 1 is a value
