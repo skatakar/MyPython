29f0a58217b8bbd9d759776a69f9fa224657ccc6
knaveen.usapp@gmail.com <sudhakar.katakara@gmail.com>
2017-09-30 17:37:56 -0500

    Operations on integers

---
+++ Basics/Operations_On_Integers.py
@@ -0,0 +1,30 @@
+
+###Doing basic maths functions
+
+a,b = 5,3
+
+def Sum():
+    print "Sum of {} and {} is {}".format(a,b,a+b)
+
+def Diff():
+    c = a-b
+    print "Diff between {} and {} is {}".format(a,b,c)
+
+def Mult():
+    c=a*b
+    print "Multiplication of {} and {} is {}".format(a,b,a*c)
+
+def Divison(a,b):
+    c = a/b
+    print "Division between {} amd {} is {}".format(a,b,c)
+
+    
+Sum()
+print "***************"
+Diff()
+print "***************"
+Mult()
+print "***************"
+Divison(6,3)
+
+
